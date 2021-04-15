4383e8f97f3203cb17c35caed7ef405c5070dcb4
Brian Shearer <rrckhunter7@gmail.com>
2021-04-15 10:06:31 -0600

    feature/timer

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+
+// function list with notes 
+// 1 start quiz button and then subsequent question buttons change to next page.
+var Qchange = document.getElementById();
+var rightA = ["qstart", "idq1", "idq2", "idq3"];
+
+
+function changePage() {
+    // for loop to go through right answer ids
+    rightA = 0;
+    for(var i = 0; i < 5; rightA.length; i++);
+
+    
+// var for getting id or class to link up to function with button and links 
+// event listener as a function call
+// maybe append child to get linked button to go to next Q or an array?
+
+}
+
+addEventListener(clicked, (changePage))
+
+function timer() {
+//     goes down 10 seconds when wrong button or link clicked
+//     var linked to elements of wrong answers I think all wrong answers could have the 
+//     same id ?
+}
+
+function wrongRightAnswer() {
+
+    if(parameter=== "rightA") {
+
+    document.querySelector("#or.").textContent = "Right Answer";
+
+    } 
+    else {
+        document.querySelector("# .").textContent = "wrong Answer";
+    }
+    // querySelector to print right or wrong answer at bottom
+    document.querySelector().text 
+    // appendChild to link above text to last Li
+    // wrong or right answer displayed under question
+    // maybe if or else statement to go from id's of right or wrong answers
+
+}
+
+function endScore() {
+//     endscore displayed and input text box for initials
+// }
+}
+
+function gameRepeat() {
+    // ask if high score to be kept and if game to be repeated
+
+}
